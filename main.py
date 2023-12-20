b422813510ed3283a827449f573f52f8f56a391c
Farhad Sadeghlo <farhad.sadeghlo@gmail.com>
2023-12-20 18:57:42 -0500

    Day 1313

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # from Day845_rotate_a_list_by_k_elements import listRotator
 # from Day852_Maximum_Subarray_Sum import max_subarray
 from Day1311_push_pop_pull import PushPopPull
+from Day1313_bitwise_AND_of_All_Integers import BitwiseAND
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -13,21 +14,24 @@ if __name__ == '__main__':
     # MonteCarlo(input('Please indicate how many iterations are needed: ')).pi_num_by_montecarlo()
     # listRotator(input('Please provide your list: '), input('Please provide rotating number: ')).rotator()
     # max_subarray(input('Please provide your list of numbers: '), input('what length of subarray do you like: ')).calc_max_subarray()
+    BitwiseAND(input('Type your number: '), input('Type your number: ')).Bitwise_AND()
 
-    # Create an instance of PushPopPull
-    my_stack = PushPopPull()
+    ############## Create an instance of PushPopPull
+    # my_stack = PushPopPull()
+    #
+    # # Push elements onto the stack
+    # my_stack.push()
+    #
+    # # Pop an element from the stack
+    # popped_element = my_stack.pop()
+    # print(f"Popped element: {popped_element}")
+    #
+    # # Peek at the top element of the stack
+    # top_element = my_stack.peek()
+    # print(f"Top element: {top_element}")
+    #
+    # # Pull an element from the stack
+    # pulled_element = my_stack.pull()
+    # print(f"Pulled element: {pulled_element}")
+    ###################
 
-    # Push elements onto the stack
-    my_stack.push()
-
-    # Pop an element from the stack
-    popped_element = my_stack.pop()
-    print(f"Popped element: {popped_element}")
-
-    # Peek at the top element of the stack
-    top_element = my_stack.peek()
-    print(f"Top element: {top_element}")
-
-    # Pull an element from the stack
-    pulled_element = my_stack.pull()
-    print(f"Pulled element: {pulled_element}")
