eeed783c75dc33f4295d88ca69cfb08dfd8fab2e
Farhad Sadeghlo <farhad.sadeghlo@gmail.com>
2023-12-23 12:56:59 -0500

    containerized and deployed with docker and waitress - in separate folder

---
+++ Day824_merge_two_binary_trees.py
@@ -0,0 +1,13 @@
+class Binary_tree:
+
+    def __init__(self, key):
+
+        # self.left = None
+        # self.right = None
+        self.key = key
+
+    def merge(self):
+
+        print(self.key)
+
+        return
